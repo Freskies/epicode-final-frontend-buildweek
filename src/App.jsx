9362b853ad0c0dd379cc04f0dddd89ffce0cb9bb
GiulioFardella <giulio.fardella@outlook.it>
2024-12-16 16:18:31 +0100

    initialize and then adds mods

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import Header from "./components/Header";
-
+import Sidebar from "./components/Sidebar";
 function App() {
 	return (
 		<>
 			<Header />
 			<main className="main">
 				<aside className="profile">
+			<Sidebar/>
 					<figure className="card"></figure>
 				</aside>
 				<section className="post-section"></section>
