437d028c0eaa4686a3e1bfb60531540bf84aaa57
GiulioFardella <giulio.fardella@outlook.it>
2024-12-18 14:31:51 +0100

    adds Experiences.jsx and various mods

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -5,6 +5,8 @@ import { setProfile } from "./../redux/action/profile";
 // TEMP
 import "./../assets/various-css/valerio.css";
 import cover from "../assets/pics/cover.jpg";
+import profilepic from "../assets/pics/profilepic.jpeg"
+import Experiences from "./Experiences";
 
 const Settings = () => {
 	// your profile
@@ -36,13 +38,14 @@ const Settings = () => {
 			<section className="my-profile">
 				<figure className="profile-card">
 					<img src={cover} alt="" className="profile-cover" />
-					<img src={profileImage} alt="" className="profile-image" />
+					<img src={profilepic} alt="" className="profile-image" />
 					<figcaption className="profile-info">
 						<p className="profile-info__name">
 							{firstName} {lastName}
 						</p>
 						<p className="profile-info__email">{email}</p>
 					</figcaption>
+                <Experiences/>
 				</figure>
 			</section>
 			<aside className="other-profiles"></aside>
