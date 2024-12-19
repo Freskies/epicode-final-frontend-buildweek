1adf1ce655afe3ccdaad736e39537601d8f0a2d5
Freskies <43386854+Freskies@users.noreply.github.com>
2024-12-19 15:06:27 +0100

    fix profile page

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,24 +1,20 @@
-import { useDispatch, useSelector } from "react-redux";
-import { deleteExperience, getExperiences } from "../fetchFunctions";
-import { setExperiences } from "./../redux/action/experiences";
-import { useCallback } from "react";
+import PropTypes from "prop-types";
+
+Experience.propTypes = {
+	experience: PropTypes.shape({
+		_id: PropTypes.string.isRequired,
+		role: PropTypes.string.isRequired,
+		company: PropTypes.string.isRequired,
+		startDate: PropTypes.string.isRequired,
+		endDate: PropTypes.string,
+		description: PropTypes.string.isRequired,
+		area: PropTypes.string.isRequired,
+	}).isRequired,
+};
 
 function Experience({
 	experience: { _id: id, role, company, startDate, endDate, description, area },
 }) {
-	const dispatch = useDispatch();
-	const setterExperiences = useCallback(
-		experiences => dispatch(setExperiences(experiences)),
-		[dispatch],
-	);
-
-	const { _id: profileId } = useSelector(({ profile }) => profile);
-	const handleDelete = async e => {
-		e.preventDefault();
-		deleteExperience(profileId, id);
-		getExperiences(profileId, setterExperiences);
-	};
-
 	return (
 		<li className="experience-item">
 			<p>
@@ -30,7 +26,7 @@ function Experience({
 			<p>{description}</p>
 			<p>{area}</p>
 
-			<button onClick={handleDelete}>Elimina</button>
+			<button data-experience-id={id}>Elimina</button>
 		</li>
 	);
 }
