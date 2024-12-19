657d6dd1e2953daa78380d3cab91c55aeb2df03e
Martino Brundu <martino.brundu@libero.it>
2024-12-19 12:35:26 +0100

    commit martino

---
+++ src/components/DropdownContent.jsx
@@ -0,0 +1,11 @@
+import "./dropdown.css"
+
+const DropdownContent = ({children}) => {
+    return (
+        <div className="dropdown-content">
+            {children}
+        </div>
+    )
+}
+
+export default DropdownContent
